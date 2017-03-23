8991d8fab68742ecd02ade92bc6b1e05264a86fe
saikrishna09333 <gorantla.saikrishna33@gmail.com>
2017-03-23 23:38:48 +0530

    First push

---
+++ cookbooks/learn_chef_httpd/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright (C) 2014
+#
+#
+#
+yum_package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
+
+service 'iptables' do
+  action :stop
+end
